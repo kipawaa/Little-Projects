9db0788a27aee8d8879f5b0f3c4c75ba8d8bf7e2
River McCubbin <mccubbinri@gmail.com>
2021-02-12 20:24:07 -0500

    updated naming for C files and began standardizing main functions in python files

--- Python/Fun With Numbers/mobius.py
+++ Python/Fun With Numbers/mobius.py
@@ -1,13 +1,16 @@
 from prime_decomposition import get_prime_decomposition
 
 def mobius(n):
+    # 1 returns 1
     if n == 1:
         return 1
 
     primes = get_prime_decomposition(n)
     
+    # if we have a prime factor with multiplicity > 1 then return 0
     if len(set(primes)) < len(primes):
         return 0
+    
     
     return (-1)**(len(primes))
 
@@ -15,5 +18,6 @@ def mobius(n):
 if __name__ == '__main__':
     start = int(input("input the start number (inclusive): "))
     end = int(input("input the end number (inclusive): "))
+    
     for i in range(start, end + 1):
         print(f"\u03BC({i}) = {mobius(i)}")
--- Python/Fun With Numbers/sigma_k.py
+++ Python/Fun With Numbers/sigma_k.py
@@ -5,6 +5,15 @@ def sigma_k(num, k):
 
 
 if __name__ == '__main__':
+    """
+    most = 0
+    for i in range(0, 150000):
+        most = max(most, sigma_k(i, -1))
+
+    print(f"the highest value we found was {most}")
+    
+    """
+    
     print("enter a number to perform sigma_k on:")
     num = int(input("number: "))
     k = int(input("k: "))
