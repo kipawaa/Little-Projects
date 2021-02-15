e066eca2b116497ed171ca5a604f19649b019ba9
River McCubbin <mccubbinri@gmail.com>
2021-02-15 14:11:45 -0500

    modified main functions to be more user-friendly

--- Python/Fun With Numbers/factors.py
+++ Python/Fun With Numbers/factors.py
@@ -1,6 +1,20 @@
 def get_factors(num):
-	return [i for i in range(1, num + 1) if num % i == 0]
+    return [i for i in range(1, num + 1) if num % i == 0]
 
 if __name__ == '__main__':
-	for i in range(1, 10):
-		print(str(i) + ": " + str(get_factors(i)))
+    mode = int(input("choose a mode:\nbatch mode: 0\nsingle mode: 1\n"))
+    
+    """ batch mode """
+    if mode == 0:
+        start = int(input("input the start number: "))
+        end = int(input("input the end number: "))
+        
+        for i in range(start, end):
+            print(f"{i} has factors: {get_factors(i)}")
+
+    if mode == 1:
+        num = 1
+        while num > 0:
+            num = int(input("input a number (or -1 to quit): "))
+            print(f"{num} has factors: {get_factors(num)}")
+
--- Python/Fun With Numbers/sigma_k.py
+++ Python/Fun With Numbers/sigma_k.py
@@ -5,7 +5,25 @@ def sigma_k(num, k):
 
 
 if __name__ == '__main__':
-    """
+    mode = int(input("input 0 for batch mode or 1 for single mode: "))
+
+    """ batch mode """
+    if mode == 0:
+        k = int(input("input k: "))
+        start = int(input("input the start number: "))
+        end = int(input("input the end number: "))
+        for i in range(start, end):
+            print(f"sigma_{k}({i}) = {sigma_k(i, k)}")
+    
+
+    """ single mode """
+    if mode == 1:
+        print("enter a number to perform sigma_k on:")
+        num = int(input("number: "))
+        k = int(input("k: "))
+        print(sigma_k(num, k))
+
+    """ find max
     most = 0
     for i in range(0, 150000):
         most = max(most, sigma_k(i, -1))
@@ -14,7 +32,4 @@ if __name__ == '__main__':
     
     """
     
-    print("enter a number to perform sigma_k on:")
-    num = int(input("number: "))
-    k = int(input("k: "))
-    print(sigma_k(num, k))
+
