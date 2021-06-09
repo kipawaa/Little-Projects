6658b14b39a402c8754fdbd61d216ced29d7b646
River McCubbin <mccubbinri@gmail.com>
2021-06-09 19:16:34 -0400

    re-worked to store cards as ints, added printing functionality. Now working on shuffling

--- C/Cards/deck.c
+++ C/Cards/deck.c
@@ -1,15 +1,79 @@
 #include"deck.h"
 
-Deck* newDeck(void) {
-        Deck* deck = (Deck*)calloc(1, sizeof(Deck));
 
-        for (int suit = 0; suit < 4; suit++) {
-                for (int val = 0; val < 13; val++) {
-                        card = newCard(suit, val);
-                        card -> next = deck -> next;
-                        deck -> next = card;
+Deck* newDeck(void) {
+        Deck* deck = (Deck*)malloc(sizeof(Deck));
+        
+        // initialize values
+        deck -> length = 52;
+        deck -> index = 0;
+
+        // fill the deck
+        for (char suit = 0; suit < 4; suit++) {
+                for (char val = 0; val < 13; val++) {
+                        deck -> cards[suit * 13 + val] = suit * 13 + val;
                 }
         }
+        
+        return deck;
+}
 
-        return Deck;
+
+int getNext(Deck* deck) {
+        // the ternary expression on the right determines the index of the first element in the queue.
+        // if that is beyond index 0, then it returns the value before it, otherwise it returns the end of the array
+        if (deck -> length == ((deck -> index > 0) ? deck -> index - 1 : 53)) {
+                printf("I'm empty!!!\n");
+                return -1;
+        }
+        int temp = deck -> cards[deck -> index];
+        deck -> index = (deck -> index) < (53 - 1) ? (deck -> index + 1) : 0; 
+        return temp;
+}
+
+
+/*
+void overhandShuffle(Deck* deck) {
+        srand(time());
+        
+        int total = 0;
+
+        while (total < deck -> length) {
+                int numCards = rand() % (deck -> length / 3);
+                
+}
+*/
+
+
+void cardToString(char* string, int numBytes, int card) {
+        int suitVal = card / 13;
+        int numVal = card % 13 + 1;
+
+        char* suit;
+        char val[3];
+
+        if (suitVal == 0) suit = "\u2660";
+        else if (suitVal == 1) suit = "\u2665";
+        else if (suitVal == 2) suit = "\u2666";
+        else if (suitVal == 3) suit = "\u2663";
+
+        if (numVal == 13) strncpy(val, "K", 3);
+        else if (numVal == 12) strncpy(val, "Q", 3);
+        else if (numVal == 11) strncpy(val, "J", 3);
+        else if (numVal == 1) strncpy(val, "A", 3);
+        else snprintf(val, 3,  "%d", numVal);
+        
+        snprintf(string, numBytes, "%s%s", val, suit);
+}
+
+
+void printDeck(Deck* deck) {
+        int i = 0;
+        while ((deck -> index + i) % 53 != deck -> length) {
+                char card[10];
+                cardToString(card, 10, deck -> cards[(deck -> index + i) % 53]);
+                printf("%s, ", card);
+                i++;
+        }
+        printf("\n");
 }
--- C/Cards/deck.h
+++ C/Cards/deck.h
@@ -1,7 +1,51 @@
-#include<stdlib.h>
-#include"card.h"
+/* Deck.h
+ **************
+ * This file implements a queue using an array.
+ * An array is used to improve lookup, enqueue and dequeue times over a linked list.
+ * modular arithmetic is used to make enqueue and dequeue more efficient.
+ * to achieve this, we store length and index.
+ * length represents the number of data entries currently in the queue
+ * index represents the current start of the queue.
+ * When an item is dequeued, it is removed from the array at index index
+ * index is then incremented, and the "start" of the queue is now the next element in the array
+ * Deqing is done by removing the item at index length
+ * length is then decremented
+ * modular arithmetic is used to ensure that both length and index remain valid indices
+ * this achieves O(1) enqueue, O(1) dequeue
+ */
 
-typedef struct Deck {
-        int length;
-        Card* next;
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+/* Struct: Deck
+ ***************
+ * Queue structure for storing a deck of cards
+ */
+typedef struct ArrayQueue {
+        int length; // how many cards are currently in the Deck
+        int index; // current index of the first Card in the Deck
+        int cards[53]; // stores the Card data as integers (need 53 to have a gap for the queue)
 } Deck;
+
+
+/* Function: newDeck
+ *******************
+ * returns a new (sorted) Deck of Cards
+ */
+Deck* newDeck(void);
+
+
+/* Function: getNext
+ *******************
+ * returns a pointer to the next card in the deck
+ */
+int getNext(Deck* deck);
+
+
+/* Function: printDeck
+ *********************
+ * prints the cards in the deck in order
+ */
+void printDeck(Deck* deck);
