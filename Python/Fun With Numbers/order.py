a61a0d1a823b3dcad69003c41c26c87b069a3943
River McCubbin <mccubbinri@gmail.com>
2021-09-02 10:18:09 -0400

    added main

--- Python/Fun With Numbers/order.py
+++ Python/Fun With Numbers/order.py
@@ -8,3 +8,20 @@ def order(a, n):
         if pow(a, k, n) == 1:
             return k
     return phi(n)
+
+
+if __name__ == "__main__":
+    user = 'a'
+    mod = 'b'
+
+    while (user != 'q' and mod != 'q'):
+        user = input("input a number to find it's order: ")
+        mod = input("input your modulus: ")
+
+        try:
+            user = int(user)
+            mod = int(user)
+            order = order(user, mod)
+            print(f"the order of {user} mod {mod} is {order}")
+        except:
+            print("please provide valid input")
