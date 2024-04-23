c8f3f335d55be7654186dcb07ee4e9e649d7ef88
River McCubbin <mccubbinri@gmail.com>
2024-04-23 13:00:20 -0400

    generalized, added progress bar for large numbers

--- Python/Fun With Numbers/Gaussian Integers/gaussian_divisors.py
+++ Python/Fun With Numbers/Gaussian Integers/gaussian_divisors.py
@@ -1,13 +1,21 @@
+from math import sqrt, ceil
+from tqdm import tqdm
+
 def get_gaussian_divisors(n):
     divs = []
 
-    for x in range(-30, 31, 1):
-        for y in range(-30, 31, 1):
+    radius = ceil(sqrt(n.real**2 + n.imag**2))
+
+    for x in tqdm(range(radius+1)):
+        for y in range(radius+1):
             if not (x == 0 and y == 0):
                 d = complex(x, y)
-                r = 30 / d
+                r = n / d
                 if int(r.real) == r.real and int(r.imag) == r.imag:
                     divs.append(d)
+                    divs.append(d*1j)
+                    divs.append(-d)
+                    divs.append(d*-1j)
     return divs
 
 if __name__ == "__main__":
@@ -15,11 +23,11 @@ if __name__ == "__main__":
 
     while (user != "q"):
         user = input("input a number to find it's gaussian divisors: ")
-        try:
-            user = complex(user)
-            divs = get_gaussian_divisors(user)
-            print(f"{user} has {len(set(divs))} gaussian divisors:")
-            for div in divs:
-                print(div)
-        except:
-            print("please provide valid input")
+        #try:
+        user = complex(user)
+        divs = get_gaussian_divisors(user)
+        for div in divs:
+            print(div)
+        print(f"{user} has {len(set(divs))} gaussian divisors, listed above")
+        #except:
+        #    print("please provide valid input")
